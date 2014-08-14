7345efa2b4c35b84c078811c664e2808da9bb41c
2bedigital <hector@2bedigital.com>
2014-08-14 11:49:07 +0200

    pipeline javascript problems

--- app/assets/javascripts/spree/frontend/spree_store_locator.js
+++ app/assets/javascripts/spree/frontend/spree_store_locator.js
@@ -1,2 +1,4 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
+//= require lib/handlebars-1.0.0
+//= require lib/jquery.storelocator
